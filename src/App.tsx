e3ebeed8648c2582ad35d7b14f3c9ca19aebdd16
Jürgen Mikk <jurgen.mikk@khk.ee>
2022-09-07 21:44:03 +0300

    Implemented dark-theme switch & configured tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import Countries from './components/Countries';
-import {FormControl, IconButton, InputBase } from '@mui/material';
+import {FormControl, IconButton } from '@mui/material';
 import { HiOutlineSearch } from 'react-icons/hi';
-import { FaRegMoon } from 'react-icons/fa';
+import { FaRegMoon, FaMoon } from 'react-icons/fa';
 
 function App() {
 
@@ -16,6 +16,7 @@ function App() {
     }]);
     const [timer, setTimer] = useState<any>(null);
     const [selected, setSelected] = useState<String>();
+    const [themeSwitch, setTheme] = useState<String>('light');
 
     const handleSelect = (e : React.ChangeEvent<HTMLSelectElement>) => {
         setSelected(e.target.value);
@@ -27,6 +28,10 @@ function App() {
         const newTimer = setTimeout(() =>
             fetchbyName(), 500);
         setTimer(newTimer);
+    }
+
+    const handleClick = () => {
+        {themeSwitch === 'light' ? setTheme('dark') : setTheme('light')};
     }
 
     const fetchbyName = async () => {
@@ -42,8 +47,8 @@ function App() {
     }
 
 return (
-        <div className="h-screen w-full">
-            <div className="flex items-center w-full h-24 border-b-2 border-gray-300">
+        <div className={`h-screen w-full ${themeSwitch === 'dark' ? "bg-slate-800 text-white": null}`}>
+            <div className={`flex items-center w-full h-24 border-b-2 border-gray-300 ${themeSwitch === 'dark' ? "bg-slate-700 border-none" : null}`}>
                 <div className="w-4/5 sm:ml-32 ml-4 sm:text-4xl text-xl font-bold">
                     <h1>
                         Where in the world?
@@ -51,29 +56,30 @@ return (
                 </div>
                 <div className="w-1/5 sm:ml-auto ml-20 text-lg sm:mr-0 mr-20">
                     <div className="inline-block sm:mr-2">
-                        <FaRegMoon/>
+                        {themeSwitch === 'dark' ? <FaMoon className="text-white"/> : <FaRegMoon/>}
                     </div>
-                    <button name="theme-switch">
+                    <button onClick={handleClick} name="theme-switch">
                         Dark
                     </button>
                 </div>
             </div>
             <div className="flex sm:flex-nowrap flex-wrap w-full h-16 mt-16">
                 <div className="w-4/5 sm:ml-44 ml-4">
-                    <InputBase
+                    <input
                         name="searchCountry"
                         placeholder="Search for a country..."
-                        className="shadow-md p-2 w-60"
+                        className={`shadow-md p-2 w-60 ${themeSwitch === 'dark' ? "text-white text-bold bg-slate-700" : null}`}
                         onChange={handleChange}
                     />
                     <IconButton sx={{ p: '10px' }} type='button' aria-label="search">
-                        <HiOutlineSearch/>
+                        {themeSwitch === 'dark' ? <HiOutlineSearch className="text-white"/> : <HiOutlineSearch/>}
                     </IconButton>
                 </div>
                 <div className="w-1/5 sm:mt-0 mt-6 sm:ml-0 ml-6">
                     <FormControl sx={{ m: 1, minWidth: 200 }} size="small">
                         <select
                             name="region"
+                            className={`p-1 rounded-md ${themeSwitch === 'dark' ? "text-white bg-slate-700" : null}`}
                             id="region"
                             onChange={handleSelect}
                         >
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
   content: ["./src/**/*.{html,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans' : ['Nunito Sans', ...defaultTheme.fontFamily.sans],
+      }
+    },
   },
   plugins: [],
 }
